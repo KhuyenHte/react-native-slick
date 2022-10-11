d9b7a327eac96631e89c35e0c4f0aba207fe87c7
= <khuyen.nguyen@htecom.vn>
2022-10-11 11:48:20 +0700

    refactor

--- src/index.js
+++ src/index.js
@@ -323,7 +323,7 @@ export default class extends Component {
     // contentOffset is not working in react 0.48.x so we need to use scrollTo
     // to emulate offset.
     if (this.initialRender && this.state.total > 1) {
-    //   this.scrollView.scrollTo({ ...offset, animated: false })
+      this.scrollView.scrollTo({ ...offset, animated: false })
       this.initialRender = false
     }
 
